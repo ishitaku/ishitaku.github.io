b58a7fa1641ffd94a114b09a64ad9effeeb8afcc
ishikura1 <ishikuratakuya00@gmail.com>
2016-10-08 17:12:21 +0900

    new file

---
+++ js/resource.js
@@ -0,0 +1,8 @@
+﻿var res = {
+    background_png : "../res/gi-gu.png",
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
---
+++ js/title.js
@@ -0,0 +1,13 @@
+﻿
+
+var gamestart = cc.Layer.extend({
+	ctor: function() {
+		this._super();
+		var size = cc.director.getWinSize();
+		var backgroundLayer = cc.Sprite.create(res.background_png);
+		backgroundLayer.setPosition(size.width,size.height /2 );
+        	this.addChild(backgroundLayer);
+	}
+	
+});
+
